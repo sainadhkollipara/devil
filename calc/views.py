20175b71dc26a7d0da7d8888ff4c08c4798c96f7
Sainadh <kolliparasainadh@gmail.com>
2021-07-28 23:31:39 +0530

    sainadh

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,4 @@
+
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
@@ -5,4 +6,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World");
+    return render(request, 'home.html',{'name':'Navin'});
+    
